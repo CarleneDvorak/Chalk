045fd77ff38780ab3cfa5703b1e66ae8b0bfc3d0
DVORAK <cd0884213@otc.edu>
2018-10-29 08:49:17 -0500

    adding ignore

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("hello World")
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hello world!'));
+
+ const chalkAnimation = require('chalk-animation');
+ 
+chalkAnimation.rainbow('Lorem ipsum dolor sit amet');
